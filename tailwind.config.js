fca6cff78a1cdb87fb4ce01518072c96a80da500
Choirul Anam <choirulanm.15@gmail.com>
2024-11-18 13:48:23 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        'h1': '80px',
+        'h2': '64px',
+        'h3': '48px',
+        'h4': '36px',
+        'h5': '24px',
+        'h6': '20px',
+        'b1': '24px',
+        'b2': '20px',
+        'b3': '16px',
+      },
+      fontFamily: {
+
+      },
+      colors: {
+        "primary-1": "#0046B1",
+        "primary-2": "#66A0D3",
+        "primary-3": "#D6E9FA",
+        "secondary-1": "#B3FD4A",
+        "secondary-2": "#D0F68E",
+        "secondary-3": "#E9F8A6",
+        "black-1": "#222222",
+        "black-2": "#4A5568",
+        "black-3": "#A0AEC0",
+        "white-1": "#FFF",
+        "white-2": "#F9F9F9",
+      },
+    },
+  },
+  plugins: [],
+};
