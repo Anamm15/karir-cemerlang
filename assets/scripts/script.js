24ba15db7c91fcaa12beafe849e665d4a12cac02
Choirul Anam <choirulanm.15@gmail.com>
2024-11-23 03:50:34 +0700

    Mengubah struktur folder, homepage revisi, gambar hero di android

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -64,20 +64,22 @@ carousel.addEventListener('touchend', (e) => {
 });
 
 const kategoriSwiper = new Swiper('.kategori__carousel', {
-    slidesPerView: 1,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        // Responsive settings
-        576: { slidesPerView: 1 },
-        768: { slidesPerView: 2 },
-        1024: { slidesPerView: 4 },
-        1440: { slidesPerView: 5 },
-        1920: { slidesPerView: 5 },
-    },
+  slidesPerView: 1,
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  loop: true,
+  watchOverflow: true,
+  breakpoints: {
+      576: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 4 },
+      1440: { slidesPerView: 5 }, 
+      1920: { slidesPerView: 5 }, 
+  },
 });
+
 
 const artikelSwiper = new Swiper('.artikel__carousel', {
     slidesPerView: 1,
