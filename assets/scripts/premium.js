416785045e2b5531fa99969abd1bb7a26dde9dfc
Choirul Anam <choirulanm.15@gmail.com>
2024-12-19 00:19:47 +0700

    Transisi navbar

--- assets/scripts/premium.js
+++ assets/scripts/premium.js
@@ -1,10 +1,22 @@
 // NAVBAR
 const hamburgerMenu = document.getElementById('hamburger-menu');
 const mobileMenu = document.getElementById('mobile-menu');
-const mobileLogo = document.getElementById('mobile-logo');
 
 hamburgerMenu.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
+    if (mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.remove('hidden');
+        setTimeout(() => {
+            mobileMenu.classList.remove('-translate-y-full', 'opacity-0');
+            mobileMenu.classList.add('translate-y-0', 'opacity-100');
+        }, 10);
+    } else {
+        mobileMenu.classList.remove('translate-y-0', 'opacity-100');
+        mobileMenu.classList.add('-translate-y-full', 'opacity-0');
+        
+        setTimeout(() => {
+            mobileMenu.classList.add('hidden');
+        }, 300);
+    }
 });
 
 const premiumSwiper = new Swiper('.layanan__premium__content', {
--- assets/scripts/profile.js
+++ assets/scripts/profile.js
@@ -1,12 +1,23 @@
 // NAVBAR
 const hamburgerMenu = document.getElementById('hamburger-menu');
 const mobileMenu = document.getElementById('mobile-menu');
-const mobileLogo = document.getElementById('mobile-logo');
 
 hamburgerMenu.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
+    if (mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.remove('hidden');
+        setTimeout(() => {
+            mobileMenu.classList.remove('-translate-y-full', 'opacity-0');
+            mobileMenu.classList.add('translate-y-0', 'opacity-100');
+        }, 10);
+    } else {
+        mobileMenu.classList.remove('translate-y-0', 'opacity-100');
+        mobileMenu.classList.add('-translate-y-full', 'opacity-0');
+        
+        setTimeout(() => {
+            mobileMenu.classList.add('hidden');
+        }, 300);
+    }
 });
-
 
 // PROFILE EDIT
 let isInputing = false;
